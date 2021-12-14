0e5049f97e409fe1fafbb66857530b9434bd10e0
Lekst-D <spektr509@gmail.com>
2021-12-14 11:39:46 +0900

    first commit

---
+++ Question 8/for.php
@@ -0,0 +1,17 @@
+<!DOCUMENT HTML>
+<html>
+<head><title>Number one</title></head>
+<body>
+    <h1>Number 2</h1>
+    <h2>Task8</h2>
+    <?php
+    for ($i = 1; $i <= 50; $i++)
+    {
+        if($i%2==1){
+        echo "$i</br>";
+        }
+    }
+    ?>
+
+</body>
+</html>
---
+++ Question 9/while.php
@@ -0,0 +1,22 @@
+<!DOCUMENT HTML>
+<html>
+<head><title>Number one</title></head>
+<body>
+    <h1>Number 2</h1>
+    <h2>Task9</h2>
+    <?php
+$counter = 0;
+$var="HELLO";
+
+$Varlength = strlen($var);
+echo "$ch  </br>";
+while($counter<$Varlength)
+{
+    $ch=$var{$counter} ;
+    echo  " $ch <br/>";
+    $counter++;
+}
+    ?>
+
+</body>
+</html>
